8689cedb8878dedc2bfa6d02c42ab16562274702
julia sheremet <juliajessicasher@gmail.com>
2018-05-09 10:32:48 -0700

    create api-keys.ts file and add to gitignore

--- src/app/listing.service.ts
+++ src/app/listing.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Listing } from './model/listing-model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class ListingService {
+  listings: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) { }
 
   getListing(){
-    
+
   }
 
 }
